33a5319460684b391933ef3ada3bfd73f14f0201
fran-man <fran.man@hotmail.co.uk>
2020-02-22 21:58:11 +0000

    Added batch sender

--- src/main/kotlin/cloudsample/controller/BaseController.kt
+++ src/main/kotlin/cloudsample/controller/BaseController.kt
@@ -3,15 +3,28 @@ package cloudsample.controller
 import cloudsample.kafka.IncomingChannels
 import cloudsample.kafka.OutgoingChannels
 import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @EnableBinding(value = arrayOf(IncomingChannels::class, OutgoingChannels::class))
-open class BaseController {
+open class BaseController(val outChannels: OutgoingChannels) {
     @RequestMapping("/", method = arrayOf(RequestMethod.GET))
     fun rootMapping(): String {
         return "Hello world"
+    }
+
+    @RequestMapping("/send", method = arrayOf(RequestMethod.GET))
+    fun sendMsgMapping(@RequestParam messagesToSend: Int): String {
+        var count = 1
+        while(count <= messagesToSend){
+            outChannels.outgoingStream().send(MessageBuilder.withPayload("This is a message: $count").build())
+            count++
+            Thread.sleep(10)
+        }
+        return "Done!"
     }
 }
